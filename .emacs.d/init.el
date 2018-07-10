1e8413f42334c0e13aacbe6fad2967570d6f6272
fargustian <aleksfers@icloud.com>
2018-07-10 22:57:38 +0500

    emacs config

---
+++ .emacs.d/init.el
@@ -0,0 +1,31 @@
+(require 'package)
+(setq package-enable-at-startup nil)
+
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives
+             '("gnu" . "https://elpa.gnu.org/packages/") t)
+
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+    (package-refresh-contents)
+    (package-install 'use-package))
+(org-babel-load-file (expand-file-name "~/.emacs.d/fargustian.org"))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (ag rubocop doom-themes neotree yaml-mode  web-mode  use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+;; Some custom hooks
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
