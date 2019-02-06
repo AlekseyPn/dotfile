b1fa348b051ea3fe74fe056b0f6de8dfb8a0ba8c
Aleksey Peresmekhin <aleksfers@icloud.com>
2019-02-06 20:23:45 +0500

    update emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,31 +1,14 @@
 (require 'package)
 (setq package-enable-at-startup nil)
-
 (add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/") t)
-(add-to-list 'package-archives
-             '("gnu" . "https://elpa.gnu.org/packages/") t)
-
+     '("melpa" . "http://melpa.org/packages/"))
+;;(add-to-list 'package-archives
+;;     '("melpa2" . "http://www.mirrorservice.org/sites/melpa.org/packages/"))
+;;(add-to-list 'package-archives
+;;     '("melpa3" . "http://www.mirrorservice.org/sites/stable.melpa.org/packages/"))
 (package-initialize)
-
 (unless (package-installed-p 'use-package)
-    (package-refresh-contents)
-    (package-install 'use-package))
-(org-babel-load-file (expand-file-name "~/.emacs.d/fargustian.org"))
+  (package-refresh-contents)
+  (package-install 'use-package))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (ag rubocop doom-themes neotree yaml-mode  web-mode  use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-;; Some custom hooks
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
+(org-babel-load-file (expand-file-name "~/.emacs.d/my_init.org"))
